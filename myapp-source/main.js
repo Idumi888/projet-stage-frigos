bdf6da98b93e3b7ad1d9b423bbbf503b26479744
Idumi888 <vincent.dievart@epsi.fr>
2020-06-14 23:23:03 +0200

    app full built

---
+++ myapp-source/main.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({ width: 1800, height: 1200 });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+
+
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
